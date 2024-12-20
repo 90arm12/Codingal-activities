aea2965130bdb028ad8b45f8624939106acce33a
90arm12 <akeel.mannoor@gmail.com>
2024-12-20 10:25:03 -0800

    Done commit

---
+++ JS OBJECTS/script.js
@@ -0,0 +1,26 @@
+var student = {
+    name:"Akeel",
+    roll:4,
+    school:"Monipur High School and College"
+};
+
+document.getElementById("student_info").innerHTML = "Name: " +student.name
++ ", Roll No: " +student.roll
++ ", School: " +student.school;
+
+//Built-in Object-Math
+document.getElementById("asin_value").innerHTML = Math.asin(-1);
+var random_number = Math.random() * 500;
+document.getElementById("random_number").innerHTML = random_number;
+document.getElementById("rounded_random_number").innerHTML = Math.round(random_number);
+
+//Built-in Objects-Arguments
+function getNumbers() {
+    var product = 1;
+    for(var index=0; index <=3; index=index+1) {
+        product=product*arguments[index];
+    }
+    return product;
+}
+var numbers= getNumbers(1,2,3,4);
+document.getElementById("numbers").innerHTML = numbers;
