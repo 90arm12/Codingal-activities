7ad6574ffece888b449c3563d687116e6aff96ab
90arm12 <akeel.mannoor@gmail.com>
2024-12-27 10:12:50 -0800

    finish commit

---
+++ HTML DOM/script.js
@@ -0,0 +1,9 @@
+var heading_text = document.getElementById("myheading").innerHTML;
+alert(heading_text);
+
+function showImage() {
+    document.getElementById("image").src ="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg";
+
+    document.getElementById("image").style.width = "300px";
+}
+document.body.style.backgroundColor = "lavender";
