af0897659eefa1a36146605a0555bbc355e2776f
90arm12 <akeel.mannoor@gmail.com>
2025-01-10 09:56:20 -0800

    detail commit

---
+++ New folder/script.js
@@ -0,0 +1,11 @@
+var str = "hello how are you doing for the day";
+var search_position = str.search("doing");
+
+var txt = str.replace("how are","how are you");
+
+var x = 70;
+var y = 90;
+
+document.getElementById("search_result").innerHTML = search_position;
+document.getElementById("replace_result").innerHTML = txt;
+document.getElementById("string_result").innerHTML = String(x)+String(y);
